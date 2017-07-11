7ebb2ee29482a85a1201cafa944fe6bb1a6e115a
jorgeboucas <mail@jorgeboucas.com>
2017-07-11 11:25:20 +0200

    config.py: fixed --config bugs

--- bit/config.py
+++ bit/config.py
@@ -157,27 +157,27 @@ def read_bitconfig(showit=None,bit_config=".bit_config"):
 
 def check_reqs(requirements,configdic,config_file=None, gitssh=None):
     if "owncloud_address" in requirements:
-        configdic["owncloud_address"]=config.get_owncloud_address()
+        configdic["owncloud_address"]=get_owncloud_address()
     if "owncloud_upload_folder" in requirements:
-        configdic["owncloud_upload_folder"]=config.get_owncloud_upload_folder()
+        configdic["owncloud_upload_folder"]=get_owncloud_upload_folder()
     if "owncloud_download_folder" in requirements:
-        configdic["owncloud_download_folder"]=config.get_owncloud_download_folder()
+        configdic["owncloud_download_folder"]=get_owncloud_download_folder()
     if "owncloud_user" in requirements:
-        configdic["owncloud_user"]=config.get_owncloud_user(config_file=config_file)
+        configdic["owncloud_user"]=get_owncloud_user(config_file=config_file)
     if "owncloud_pass" in requirements:
-        configdic["owncloud_pass"]=config.get_owncloud_pass(config_file=config_file)
+        configdic["owncloud_pass"]=get_owncloud_pass(config_file=config_file)
     if "github_address" in requirements:
-        configdic["github_address"]=config.get_github_address()
+        configdic["github_address"]=get_github_address()
     if "github_organization" in requirements:
-        configdic["github_organization"]=config.get_github_organization()
+        configdic["github_organization"]=get_github_organization()
     if "github_user" in requirements:
-        configdic["github_user"]=config.get_github_user(config_file=config_file,gitssh=gitssh )
+        configdic["github_user"]=get_github_user(config_file=config_file,gitssh=gitssh )
     if "github_pass" in requirements:
-        configdic["github_pass"]=config.get_github_pass(config_file=config_file,gitssh=gitssh )
+        configdic["github_pass"]=get_github_pass(config_file=config_file,gitssh=gitssh )
     if "local_path" in requirements:
         configdic["local_path"]=get_local_path()
     if "user_group" in requirements:
-        configdic["user_group"]=config.get_user_group()
+        configdic["user_group"]=get_user_group()
     return configdic
 
 def init_user(path_to_project,github_address,github_organization,github_repo,github_user=None,github_pass=None,gitssh=None):
