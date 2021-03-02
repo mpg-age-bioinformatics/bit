e149dec7db55d86164444bf54fbd29c280d17f09
jorgeboucas <mail@jorgeboucas.com>
2021-03-02 18:18:48 +0100

    automation; v0.2.0

---
+++ bit/automation.py
@@ -0,0 +1,347 @@
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+
+import re
+
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import requests
+import json
+import sys
+
+import os
+
+from bit.config import read_bitconfig
+
+groups_dic={"Adam_Antebi":"AA",\
+"Aleksandra_Filipovska":"AF",\
+"Bioinformatics":"bit",\
+"Constantinos_Demetriades":"CD",\
+"CRISPR_Screening":"CS",\
+"Dario_Valenzano":"DV",\
+"Ivan_Matic":"IM",\
+"James_Stewart":"JS",\
+"Lena_Pernas":"LPe",\
+"Linda_Partridge":"LP",\
+"Martin_Denzel":"MD",\
+"Martin_Graef":"MG",\
+"Metabolomics":"met",\
+"Nils_Larson":"NL",\
+"Peter_Tessarz":"PT",\
+"Phenotyping":"Ph",\
+"Proteomics":"Prot",\
+"Ron_Jachimowicz":"RJ",\
+"Sara_Wickstroem":"SW",\
+"Thomas_Langer":"TL"}
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Proba
+
+def read_config(config_file="/beegfs/group_bit/data/projects/departments/Bioinformatics/bit_automation/automation.config"):
+    config_dic={}
+    with open(config_file, "r") as file_in:
+        for line in file_in:
+            line=line.rstrip("\n")
+            line=line.split("=")
+            config_dic[line[0]]=line[1]
+            
+    bit_config=read_bitconfig()
+    config_dic["github_user"]=bit_config["github_user"]
+    config_dic["github_pass"]=bit_config["github_pass"]
+    config_dic["DAVIDUSER"]=bit_config["DAVIDUSER"]
+
+    return config_dic
+    
+def send_email(subject, body="", EMAIL_TOKEN=None, \
+               attach=None, \
+               toaddr=[],\
+               fromaddr="automation@age.mpg.de",\
+               project_type=None,\
+               config_dic=None):
+    if not EMAIL_TOKEN:
+        EMAIL_TOKEN=config_dic["EMAIL_TOKEN"]
+    if not project_type:
+        project_type=config_dic["project_type"]
+
+    msg = MIMEMultipart()
+    
+    static_receiver=["jboucas@age.mpg.de"]
+    toaddr=",".join(static_receiver+toaddr)
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    
+    if str(subject)[0] != "[":
+        subject=" "+subject
+    
+    msg['Subject'] = "[{project_type}]{subject}".format(project_type=project_type,subject=subject)
+    msg.attach(MIMEText(body, 'plain'))
+
+    if attach:
+        attachment = open(str(attach), "rb")
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % report_file)
+        msg.attach(part)
+
+    server = smtplib.SMTP_SSL('mail.age.mpg.de', 465)
+    server.login(fromaddr, EMAIL_TOKEN)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+    print( "Email sent to {toaddr}".format(toaddr=toaddr) )
+    sys.stdout.flush()
+    
+def check_email(email,config_dic,submission_file):
+    email=str(email).rstrip().lstrip()
+    email=email.split(",")
+    email=[ re.search("([^@|\s]+@[^@]+\.[^@|\s]+)",e,re.I) for e in email ]
+    email=[ e.group(1) for e in email if e ]
+    if not email :
+        print("Contact email not provided." )
+        sys.stdout.flush()
+        send_email("contact email not provided.",\
+                   "For {submission_file} contact email not provided.".format(submission_file=submission_file),\
+                    config_dic=config_dic)
+        sys.exit(1)
+    return email
+
+def check_group(group,submission_file,email):
+    if group not in list(groups_dic.keys()):
+        print("Group {group} does not exist.".format(group=group) )
+        sys.stdout.flush()
+        send_email("group does not exist",\
+                   "For {submission_file} group {group} does not exist.".format(submission_file=submission_file,group=group),\
+                   toaddr=email, 
+                  EMAIL_TOKEN=config_dic["EMAIL_TOKEN"],
+                  project_type=project_type)
+        sys.exit(1)
+    return True
+
+def check_project_exists(folder,config_dic,group,project_title, email):
+    if os.path.isdir(folder):
+        send_email("[{group_prefix}_{project_title}] project already exists".format(group_prefix=groups_dic[group],project_title=project_title),\
+                   "{project_title} already exists. Please submit again using a different project name.\n{folder}".format(project_title=project_title,folder=folder),\
+                   toaddr=email, 
+                   config_dic=config_dic  )
+        print("{folder} already exists".format(folder=folder))
+        sys.stdout.flush()
+        sys.exit(1)
+    return True
+
+def check_source_files(files,md5file,store_age_folder,GET_RAW,project_title,email,config_dic):
+    for f in files:
+        if ( not os.path.exists(store_age_folder+f) ) & ( GET_RAW ):
+            send_email("[{group_prefix}_{project_title}] raw data file missing".format(group_prefix=groups_dic[metadata["Group"]],project_title=project_title),\
+                       "{f} missing".format(f=f),\
+                       toaddr=email, 
+                       config_dic=config_dic)
+            print("{f} missing".format(f=store_age_folder+f))
+            sys.stdout.flush()
+            sys.exit(1)
+     
+    if md5file:
+        if ( not os.path.exists(store_age_folder+md5file) ) & ( GET_RAW ):
+            send_email("[{group_prefix}_{project_title}] md5sums file missing".format(group_prefix=groups_dic[metadata["Group"]],project_title=metadata["Project title"]),\
+                       "{f} missing".format(f=f),\
+                       toaddr=email, 
+                       config_dic=config_dic)
+            print("{f} missing".format(f=f))
+            sys.stdout.flush()
+            sys.exit(1)
+    return True
+
+
+def verify_md5sum(md5sums,md5file_in):
+    for file in list(md5sums.keys()):
+        with open(md5file_in, "r") as md5file:
+            md5check=False
+            for line in md5file:
+                if file in line:
+                    if md5sums[file] in line:
+                        md5check=True
+            if not md5check:
+                return file
+            
+    return False
+
+def md5sumcheck(file,config_dic,group,project_title,project_archive,email,md5file):
+    cmd = [ "md5sum", file ] 
+    sys.stdout.flush()
+    out=Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    re=str( out.communicate()[0].rstrip()) 
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+    md5sum=str(re).split(" ")[0].split("'")[1]
+    md5sums={ os.path.basename(file): md5sum }
+    
+    missing_file=verify_md5sum(md5sums,project_archive+md5file)
+    if missing_file:
+        send_email("[{group_prefix}_{project_title}] md5sum could not be comfirmed".format(group_prefix=groups_dic[group],project_title=project_title),\
+                   "{file} missing".format(file=missing_file),\
+                   toaddr=email, 
+                   config_dic=config_dic)
+        print("{file} md5 missing".format(file=missing_file))
+        sys.stdout.flush()
+        sys.exit(1)
+    return True
+
+def make_github_repo( repo_name, user, token, config_dic):
+    url = 'https://github.molgen.mpg.de/api/v3/orgs/mpg-age-bioinformatics/repos'
+    repo = { "name" : repo_name , \
+             "private" : 'true' ,\
+             "auto_init": 'true' }
+    response = requests.post( url, data=json.dumps(repo), auth=( user, token ))#, headers=headers)
+    if response.status_code == 201:
+        print('Successfully created Repository "%s"' % repo_name )
+
+    else:
+        print('Could not create Repository "%s"' % repo_name)
+        print('Response:', response.content)
+        send_email("[{repo_name}] repository could not be created".format(repo_name=repo_name), 
+                      config_dic=config_dic)
+        sys.stdout.flush()
+        sys.exit(1)
+    return response
+
+def make_github_issue(title, repo_name, user, token, config_dic, body=None, assignee=None ):
+    '''Create an issue on github.com using the given parameters.'''
+    # Our url to create issues via POST
+    url = 'https://github.molgen.mpg.de/api/v3/repos/mpg-age-bioinformatics/{repo_name}/issues'.format(repo_name=repo_name)
+    issue = {'title': title,\
+            'assignee': assignee}
+    # Add the issue to our repository
+    response = requests.post( url, data=json.dumps(issue), headers={"Accept": "application/vnd.github.v3+json"}, auth=( user, token ))#, headers=headers)
+    
+    if response.status_code == 201:
+        print('Successfully created Issue "%s"' % title )
+
+    else:
+        print('Could not create Issue "%s"' % title)
+        print('Response:', response.content)
+        print(response.text)
+        send_email("[{repo}] could not create Issue".format(repo=repo), 
+                        config_dic=config_dic)
+        sys.stdout.flush()
+        sys.exit(1)
+    return response
+
+def git_clone(local_name,github_repo):
+    git="git@github.molgen.mpg.de:mpg-age-bioinformatics/{github_repo}.git".format(github_repo=github_repo)
+    if not os.path.exists(local_name):
+        os.makedirs(local_name)
+        out=subprocess.call(['setfacl', '-Rdm', 'g:group_bit:rx', local_name ])
+
+    cwd = os.getcwd()
+    os.chdir(local_name)
+    out=subprocess.call(['git','init'])
+    out=subprocess.call(['git','config','remote.origin.url',git])
+    out=subprocess.call(['git','config','branch.master.remote','origin'])
+    out=subprocess.call(['git','config','branch.master.merge','refs/heads/master'])
+    out=subprocess.call(['git','pull', git])
+    os.chdir(cwd)
+    return out
+
+def git_fetch(github_repo):
+    git="git@github.molgen.mpg.de:mpg-age-bioinformatics/{github_repo}.git".format(github_repo=github_repo)
+    call=["git","fetch",git]
+    out=Popen(call, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    print(str( out.communicate()[0].rstrip()) )
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+
+def git_merge(message):
+    call=["git","merge","FETCH_HEAD","-m",message]
+    out=Popen(call, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    print(str( out.communicate()[0].rstrip()) )
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+
+def git_pull(github_repo):
+    git="git@github.molgen.mpg.de:mpg-age-bioinformatics/{github_repo}.git".format(github_repo=github_repo)
+    call=["git","pull",git]
+    out=Popen(call, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    print(str( out.communicate()[0].rstrip()) )
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+
+def git_add():
+    call=["git","add","-A", "." ]
+    out=Popen(call, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    print(str(out.communicate()[0].rstrip()) )
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+
+def git_commit(message):
+    call=["git","commit","-m", message]
+    out=Popen(call, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    print(str( out.communicate()[0].rstrip()) )
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+
+def git_push(github_repo):
+    git="git@github.molgen.mpg.de:mpg-age-bioinformatics/{github_repo}.git".format(github_repo=github_repo)
+    call=["git","push",git,"--all"]
+    out=Popen(call, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    print(str( out.communicate()[0].rstrip()) )
+    out.stdout.close()
+    out.stdin.close()
+    out.stderr.close()
+    try:
+        out.kill()
+    except:
+        pass
+
+def git_sync(local_name, github_repo, message):
+    cwd = os.getcwd()
+    os.chdir(local_name)
+    git_add()
+    git_commit(message)
+    git_fetch(github_repo)
+    git_merge(message)
+    git_push(github_repo)
+    os.chdir(cwd)
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name = 'bit',
-      version = '0.1.0',
+      version = '0.2.0',
       description = '[b]ermuda [i]nformation [t]riangle',
       url = 'https://github.com/mpg-age-bioinformatics/bit',
       author = 'Bioinformatics Core Facility of the Max Planck Institute for Biology of Ageing',
       author_email = 'bioinformatics@age.mpg.de',
       license = 'MIT',
       packages = [ 'bit' ],
-      install_requires = [ "requests >= 2.0.1","six"],
+      install_requires = [ "requests >= 2.0.1","six", "xlrd==1.2.0", "openpyxl==3.0.5"],
       zip_safe = False,
       entry_points = {'console_scripts': ['bit=bit.__init__:main']}
       )
