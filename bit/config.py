14a7e86786c7af6f0ff2032105f0df4492928407
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-07-13 14:00:23 +0200

    no need for wiki in user init

--- bit/config.py
+++ bit/config.py
@@ -185,5 +185,8 @@ def init_user(path_to_project,github_address,github_organization,github_repo,git
     response=git.git_clone(path_to_project+"/scripts."+user_name , github_address, github_organization, github_repo, github_user=github_user, github_pass=github_pass, gitssh=gitssh)
     response=git.git_clone(path_to_project+"/wiki."+user_name , github_address, github_organization, github_repo+".wiki", github_user=github_user, github_pass=github_pass, gitssh=gitssh)
     if response == 1:
-        input("\n\n*************\n\nThe wiki for this project has not yet been created.\n\n Please go to %s/%s/%s/wiki and click on 'Create the first page' and then 'Save Page'.\n\nPress Enter once you have saved the first wiki page.\n\n*************\n\n" %(github_address,github_organization,github_repo) )
+        input("\n\n*************\n\nThe wiki for this project has not yet been created.\n\n Please go to %s/%s/%s/wiki and click on 'Create the first page' and then 'Save Page'.\n\nPress Enter once you have saved the first wiki page.\n\nOtherwise press enter to skipp wiki creation.\n\n*************\n\n" %(github_address,github_organization,github_repo) )
         response=git.git_clone(path_to_project+"/wiki."+user_name ,github_address,github_organization,github_repo+".wiki",github_user=github_user,github_pass=github_pass,gitssh=gitssh)
+        if response == 1:
+            print("Skipping wiki creation.")
+            sys.stdout.flush()
