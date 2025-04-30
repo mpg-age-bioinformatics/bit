13e9d4dab9e0adcd68e16588c57c7b42bd9494c8
Jorge Boucas <jorgeboucas@users.noreply.github.com>
2025-04-30 16:10:52 +0200

    Update automation.py

--- bit/automation.py
+++ bit/automation.py
@@ -41,7 +41,8 @@ groups_dic={"Adam_Antebi":"AA",\
 "Thomas_Langer":"TL",\
 "Zachary_Frentz":"ZF",\
 "Hans_Sprenger":"HS",\
-"Hannah_Scheiblich":"HSc"}
+"Hannah_Scheiblich":"HSc"\
+"Ina_Huppertz":"IH" }
 
 def isnotebook():
     try:
